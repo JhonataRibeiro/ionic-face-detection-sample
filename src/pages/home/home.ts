fe437f7c09389c84000b50ba5cbce1bd8c4e0c00
Jhonata <jhonata@jribeiro.maisvida.med.br>
2018-01-12 17:54:05 -0200

    init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,7 +8,30 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+    this.init();
+  }
+
+  init() {
+    var win: any = window;
+    if (win.navigator) {
+      console.log('have');
+      navigator.getUserMedia({ video: true },
+        function (stream) {
+          console.log('stream', stream);
+          var video = document.querySelector('video');
+          console.log('stream 1');
+          video.setAttribute('src', URL.createObjectURL(stream));
+          console.log('stream 2');
+        }, function (err) {
+          console.log("err: ", err);
+        }
+      );
+    } else {
+      console.log('phonertc is not defined');
+    }
 
   }
 
 }
+
+
